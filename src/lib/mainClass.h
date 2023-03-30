04dbc24c3bde5b7de781de2c4dcd7a5890002ef0
Nikita Tomashevsky <niktomas19@gmail.com>
2023-03-30 20:48:49 +0300

    Убрал енабле иф

--- src/lib/mainClass.h
+++ src/lib/mainClass.h
@@ -5,12 +5,14 @@
 #define ____type_size size_t
 #endif
 
+
 template<typename Type>
 constexpr bool FLAG = std::is_same<Type, int>::value or std::is_same<Type, double>::value or std::is_same<Type, float>::value;
 
 
-template<typename Type, class=std::enable_if<FLAG<Type>>>
+template<typename Type>
 class connectedList {
+    static_assert(FLAG<Type>, " ");
 protected:
     struct Node {
         Node* next;
@@ -21,8 +23,8 @@ public:
     ____type_size length();
 };
 
-template<typename Type, class=std::enable_if<FLAG<Type>>>
-____type_size connectedList<Type, class=std::enable_if<FLAG<Type>>>::length()
+template<typename Type>
+____type_size connectedList<Type>::length()
     {
     return this->size;
 }
