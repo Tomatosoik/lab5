558a086b1098bec4167758fe79b72e9002cf2c9d
Pavel Voronov <pavel_voronov_2005@mail.ru>
2023-03-30 22:31:59 +0300

    Форматировал нахер

--- src/lib/mainClass.h
+++ src/lib/mainClass.h
@@ -5,26 +5,25 @@
 #define ____type_size size_t
 #endif
 
+template <typename Type>
+constexpr bool FLAG =
+    std::is_same<Type, int>::value or std::is_same<Type, double>::value or
+    std::is_same<Type, float>::value;
 
-template<typename Type>
-constexpr bool FLAG = std::is_same<Type, int>::value or std::is_same<Type, double>::value or std::is_same<Type, float>::value;
+template <typename Type> class connectedList {
+  static_assert(FLAG<Type>, " ");
 
-
-template<typename Type>
-class connectedList {
-    static_assert(FLAG<Type>, " ");
 protected:
-    struct Node {
-        Node* next;
-        Type inf;
-    } *head, *tail;
-    ____type_size size;
+  struct Node {
+    Node *next;
+    Type inf;
+  } *head, *tail;
+  ____type_size size;
+
 public:
-    ____type_size length();
+  ____type_size length();
 };
 
-template<typename Type>
-____type_size connectedList<Type>::length()
-    {
-    return this->size;
+template <typename Type> ____type_size connectedList<Type>::length() {
+  return this->size;
 }
